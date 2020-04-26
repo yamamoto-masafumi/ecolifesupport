9493d8d439faec1e04ddb2fbf267afa0389754ca
watairie <63224502+watairie@users.noreply.github.com>
2020-04-26 21:16:40 +0900

    お問い合わせフォーム作成(採用情報ページの後)

---
+++ themes/ECO-LifeSupport/contact.php
@@ -0,0 +1,73 @@
+<?php
+/*
+ * Template Name: お問い合わせ
+ */
+?>
+
+<?php get_header(); ?>
+
+<div class="absolute">
+<div id="content">
+  <div id="inner-content">
+      <main id="main" class="main" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+
+        <div class="top-band">
+          <h3>お問い合わせ</h3>
+        </div>
+
+        <div class="contact-contents">
+          <div class="inner-contact">
+            <br><br>
+            <div class="contact-form">
+            <?php if(wp_is_mobile()) : ?>
+              <?php echo do_shortcode( '[contact-form-7 id="93" title="contact"]' ); ?>
+            <?php else : ?>
+              <br>
+              <?php echo do_shortcode( '[contact-form-7 id="92" title="contact_pc"]' ); ?>
+              <?php endif; ?>
+            </div>
+
+        </div>
+        </div>
+
+    </main>
+  </div>
+</div>
+</div>
+
+<footer class="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
+        <div id="inner-footer" class="cf footer-top">
+          <div class="footer-top-inner">
+            <div class="footer-top-inner-info">
+              <div id="logo" class="logo" itemscope itemtype="http://schema.org/Organization">
+                <a href="<?php echo home_url(); ?>" rel="nofollow">
+                  <img src="<?php echo get_bloginfo('template_directory'); ?>/library/images/logo.svg">
+                </a>
+              </div>
+              <ul class="address">
+                <li>〒144-0052</li>
+                <li>東京都大田区蒲田5-21-13</li>
+                <li>ペガサスステーションプラザ蒲田B2</li>
+              </ul>
+            </div>
+            <nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
+              <?php
+                wp_nav_menu(array(
+                'menu' => 'FooterMenu',
+                'menu_class' => 'menu',
+                'container' => 'div',
+                ));
+              ?>
+            </nav>
+          </div>
+        </div>
+
+        <p class="source-org copyright">Copyright &copy; <?php bloginfo( 'name' ); ?> All Rights Reserved.</p>
+			</footer>
+
+		<?php // all js scripts are loaded in library/bones.php ?>
+	</body>
+</html> <!-- end of site. what a ride! -->
+
+<?php get_footer(); ?>
+</div>
