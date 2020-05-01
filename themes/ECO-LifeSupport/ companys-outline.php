3c75bbf0dc8b527810111a8c198fdbe4d1323691
yasutoikeda <ikeda.14.8310@icloud.com>
2020-05-01 17:26:48 +0900

    Contact,FAQ,修正

--- themes/ECO-LifeSupport/ companys-outline.php
+++ themes/ECO-LifeSupport/ companys-outline.php
@@ -15,80 +15,42 @@
         </div>
         <div class="company-contents">
           <div class="company-inner">
-            <div class="border"></div>
-              <div class="text">
-                <ul>
-                  <li class="item">商号</li>
-                  <li class="itemtext">エコライフサポート</li>
-                </ul>
-              </div>
-              <div class="bordergreen"></div>
-              <div class="text">
-                <ul>
-                  <li class="item">
-                  所在地
-                  </li>
-                  <li class="itemtext">
-                  〒144-0052　東京都大田区蒲田5-21-13 ペガサスステーションプラザ蒲田B2
-                  </li>
-                </ul>
-              </div>
-              <div class="bordergreen"></div>
-              <div class="text">
-                <p class="map-title">地図</p>
-                <div class="ggmap"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3245.6651308539867!2d139.71691381507773!3d35.561971644111004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60186054e604807f%3A0x47e07f8d5967034f!2z44Ko44Kz44Op44Kk44OV44K144Od44O844OI!5e0!3m2!1sja!2sjp!4v1587992934989!5m2!1sja!2sjp" width="100%" height="400px" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></div>
-              </div>
-              <div class="bordergreen"></div>
-              <div class="text">
-                <ul>
-                  <li class="item">
-                  電話番号
-                  </li>
-                  <li class="itemtext2">
-                  0120-994-838
-                  </li>
-                </ul>
-              </div>
-              <div class="bordergreen"></div>
-              <div class="text">
-                <ul>
-                  <li class="item">
-                  URL
-                  </li>
-                  <li class="itemtext2">
-                  https://ecolifesupport.com/
-                  </li>
-                </ul>
-              </div>
-              <div class="bordergreen"></div>
-              <div class="text">
-                <ul>
-                  <li class="item">
-                  E-mail
-                  </li>
-                  <li class="itemtext2">
-                  info@ecolifesupport.com
-                  </li>
-                </ul>
-              </div>
-              <div class="bordergreen"></div>
-              <div class="text">
-                <ul>
-                  <li class="item">
-                  事業内容	
-                  </li>
-                  <li class="itemtext2">
-                  粗大ゴミ処分・不要品回収・遺品整理・ゴミ屋敷・引越し・出張買取・リサイクル販売
-                  </li>
-                </ul>
-              </div>
-              <div class="bordergreen"></div>
-            </div>
+              <ul>
+                <li>
+                  <h4>商号</h4>
+                  <p>エコライフサポート</p>
+                </li>
+                <li>
+                  <h4>所在地</h4>
+                  <p>〒144-0052 <br class="pc-hidden"> 東京都大田区蒲田5-21-13 ペガサスステーションプラザ蒲田B2</p>
+                </li>
+                <li>
+                  <h4>地図</h4>
+                  <div class="ggmap"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3245.6651308539867!2d139.71691381507773!3d35.561971644111004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60186054e604807f%3A0x47e07f8d5967034f!2z44Ko44Kz44Op44Kk44OV44K144Od44O844OI!5e0!3m2!1sja!2sjp!4v1587992934989!5m2!1sja!2sjp" width="215px" height="215px" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></div>
+                </li>
+                <li>
+                  <h4>電話番号</h4>
+                  <p>0120-994-838</p>
+                </li>
+                <li>
+                  <h4>URL</h4>
+                  <p>https://ecolifesupport.com/</p>
+                </li>
+                <li>
+                  <h4>E-mail</h4>
+                  <p>info@ecolifesupport.com</p>
+                </li>
+                <li>
+                  <h4>事業内容</h4>
+                  <p>粗大ゴミ処分・不要品回収・遺品整理・ゴミ屋敷・引越し・出張買取・リサイクル販売</p>
+                </li>
+              </ul>
           </div>
-        </div >
+        </div>
       </main>
   </div>
-  <div id="inner-footer" class="cf footer-top">
+</div>
+<div id="inner-footer" class="cf footer-top">
   <div class="footer-top-inner">
     <div class="footer-top-inner-info">
       <div id="logo" class="logo" itemscope itemtype="http://schema.org/Organization">
@@ -113,7 +75,6 @@
     </nav>
   </div>
   <p class="source-org copyright">Copyright &copy; <?php bloginfo( 'name' ); ?> All Rights Reserved.</p>
-</div>
 </div>
 
 
